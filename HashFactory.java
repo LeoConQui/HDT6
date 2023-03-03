e0c93a266948782e32e7953161e9be80eb3dcdbe
LeoConQui <con18797@uvg.edu.gt>
2023-03-03 17:34:22 -0600

    Agregar SingleLinkedList con su interfaz y clase abstracta

--- HashFactory.java
+++ HashFactory.java
@@ -1,4 +1,7 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * HashFactory es la clase que implementa el patron creacional factory para retornar instancias de Hashmap, Treemap y Linkedhashmap
@@ -7,8 +10,30 @@ import java.util.Map;
  */
 public class HashFactory {
 
-    public Map<String, SingleLinkedList<String>> name() {
+    public Map<String, SingleLinkedList<String>> InstanceCreator(int opcionmapa) {
+        // creamos una referencia de tipo Map
+        Map<String, SingleLinkedList<String>> mapaaretornar;
+
+        switch (opcionmapa) {
+            case 1:
+                mapaaretornar = new HashMap<>();
+                break;
+            
+            case 2:
+                mapaaretornar = new TreeMap<>();
+                break;
+            
+            case 3:
+                mapaaretornar = new LinkedHashMap<>();
+                break;
         
+            default:
+                mapaaretornar = null;
+                break;
+        }
+
+        return mapaaretornar;
+
     }
     
 }
