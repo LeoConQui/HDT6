243dd316df97e8b370a181b7dcf1ddd2cc80c80b
LeoConQui <con18797@uvg.edu.gt>
2023-03-03 08:37:16 -0600

    HashFActory

---
+++ HashFactory.java
@@ -0,0 +1,14 @@
+import java.util.Map;
+
+/**
+ * HashFactory es la clase que implementa el patron creacional factory para retornar instancias de Hashmap, Treemap y Linkedhashmap
+ * @author Leonel Contreras 18797
+ * @version 1.0
+ */
+public class HashFactory {
+
+    public Map<String, SingleLinkedList<String>> name() {
+        
+    }
+    
+}
